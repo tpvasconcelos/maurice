917cb7c88017c90fac81bb24eb72c9f5cb95d639
Tomas Pereira de Vasconcelos <tomas@tiqets.com>
2021-01-11 21:36:19 +0100

    implement _run_before and _run_after API

--- maurice/patchers/core.py
+++ maurice/patchers/core.py
@@ -1,6 +1,7 @@
 import logging
 from abc import ABCMeta, abstractmethod
 from functools import partial
+from typing import Any, Optional
 
 import dill
 import wrapt
@@ -13,14 +14,35 @@ logger = logging.getLogger(__name__)
 
 
 class BaseMethodWrapper(metaclass=ABCMeta):
+    """
+    Notes:
+        - The `__method` attribute should remain a `private` attribute. I cannot foresee the need to access `__method`
+        from a subclass of  `BaseMethodWrapper`.
+        - The `_args` and `_kwargs` methods belong to the private API and therefore accessible to all subclasses of
+        `BaseMethodWrapper`. This way, you are able to overwrite or mutate them before a method call.
+    """
     def __init__(self, method: BoundMethodType, args: tuple, kwargs: dict):
-        self._method: BoundMethodType = method
+        self.__method: BoundMethodType = method
         self._args: tuple = args
         self._kwargs: dict = kwargs
 
-    @abstractmethod
-    def run_wrapped(self) -> BoundMethodReturnType:
-        pass
+    @property
+    def _instance(self) -> BoundMethodInstanceType:
+        return self.__method.__self__
+
+    # noinspection PyMethodMayBeStatic
+    def _run_before(self) -> bool:
+        return True
+
+    # noinspection PyMethodMayBeStatic
+    def _run_after(self, result: Optional[BoundMethodReturnType]) -> Any:
+        return result
+
+    def run(self) -> Any:
+        result = None
+        if self._run_before():
+            result = self.__method(*self._args, **self._kwargs)
+        return self._run_after(result=result)
 
 
 class CachingMethodWrapper(BaseMethodWrapper):
@@ -44,10 +66,6 @@ class CachingMethodWrapper(BaseMethodWrapper):
         self._path_to_state = self._path_to_cached_method.joinpath("state.dill")
         self._path_to_result = self._path_to_cached_method.joinpath("result.dill")
 
-    @property
-    def _instance(self) -> BoundMethodInstanceType:
-        return self._method.__self__
-
     def _get_instance_state(self) -> dict:
         if hasattr(self._instance, "__getstate__"):
             state: dict = getattr(self._instance, "__getstate__")()
@@ -61,15 +79,16 @@ class CachingMethodWrapper(BaseMethodWrapper):
         else:
             self._instance.__dict__.update(state)
 
-    def run_wrapped(self) -> BoundMethodReturnType:
-        if not self._path_to_cached_method.exists():
-            result: BoundMethodReturnType = self._method(*self._args, **self._kwargs)
+    def _run_before(self) -> bool:
+        return not self._path_to_cached_method.exists()
+
+    def _run_after(self, result: Optional[BoundMethodReturnType]) -> Any:
+        if result:
             logger.info(f"Saving cache to: {self._path_to_cached_method}")
             self._path_to_cached_method.mkdir(parents=True, exist_ok=False)
             if self._save_state:
                 self._path_to_state.write_bytes(dill.dumps(self._get_instance_state()))
             self._path_to_result.write_bytes(dill.dumps(result))
-            return result
         else:
             logger.info(f"Loading cache from: {self._path_to_cached_method}")
             if self._save_state:
@@ -79,9 +98,9 @@ class CachingMethodWrapper(BaseMethodWrapper):
 
 
 def _caching_method_wrapper(
-    method: BoundMethodType, instance: BoundMethodInstanceType, args: tuple, kwargs: dict, save_state: bool
+    method: BoundMethodType, _: BoundMethodInstanceType, args: tuple, kwargs: dict, save_state: bool
 ) -> BoundMethodReturnType:
-    return CachingMethodWrapper(method=method, args=args, kwargs=kwargs, save_state=save_state).run_wrapped()
+    return CachingMethodWrapper(method=method, args=args, kwargs=kwargs, save_state=save_state).run()
 
 
 def patch_method_by_name(name: str, cls: type) -> None:
